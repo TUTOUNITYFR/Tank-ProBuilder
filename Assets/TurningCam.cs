69c15446cf82b768d0c44ff479f6dda405842320
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2019-05-17 21:06:52 +0200

    Initial commit

---
+++ Assets/TurningCam.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TurningCam : MonoBehaviour
+{
+    public Transform target;
+ 
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
